4f615504d3d01a9ff10ec4916c78a9b07304b1ad
JackCHG <gongjackiegong@gmail.com>
2022-11-23 14:15:35 -0800

    Removed close element from lightbox

--- portfolio/index.js
+++ portfolio/index.js
@@ -6,18 +6,14 @@ Array.from(images).forEach((image) => {
   image.addEventListener("click", () => {
     let src = image.src;
     let lightboxImage = document.getElementsByClassName("lightbox-image")[0];
-    let lightboxClose = document.getElementsByClassName("lightbox-close")[0];
     lightbox.classList.add("lightbox-display");
     lightboxImage.classList.add("lightbox-display-image");
     lightboxImage.src = src;
-    lightboxClose.classList.add("lightbox-display-close");
   });
 });
 
 lightbox.addEventListener("click", () => {
   let lightboxImage = document.getElementsByClassName("lightbox-image")[0];
-  let lightboxClose = document.getElementsByClassName("lightbox-close")[0];
   lightbox.classList.remove("lightbox-display");
   lightboxImage.classList.remove("lightbox-display-image");
-  lightboxClose.classList.remove("lightbox-display-close");
 });
