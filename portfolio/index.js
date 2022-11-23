27dfac84a175821c3c114c6550d096969d6d5653
JackCHG <gongjackiegong@gmail.com>
2022-11-23 04:19:44 -0800

    Added additional console log statements

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,6 +1,7 @@
 const enlargeImage = (event) => {
   img = event.target;
   console.log(event.target);
+  console.log("Test");
   img.style.transform = "scale(1.5)";
   img.style.transition = "transform 0.25s ease";
 };
