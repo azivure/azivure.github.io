f91ac6bfbd7edd4a5cb637390e301272a6aec5ee
JackCHG <gongjackiegong@gmail.com>
2022-11-23 14:01:14 -0800

    Added javascript to support lightbox

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,10 +1,23 @@
 let images = document.getElementsByClassName("image");
+let lightbox = document.getElementsByClassName("lightbox-target")[0];
+
 Array.from(images).forEach((image) => {
   console.log("Testing");
-  image.addEventListener('click', (event) => {
-    // console.log(event.target);
-    let img = event.target;
-    img.style.transform = "scale(1.5)";
-    img.style.transition = "transform 0.25s ease";
+  image.addEventListener("click", () => {
+    let src = image.src;
+    lightbox.src = src;
+    let lightboxImage = document.getElementsByClassName("lightbox-image")[0];
+    let lightboxClose = document.getElementsByClassName("lightbox-close")[0];
+    lightbox.classList.add("lightbox-display");
+    lightboxImage.classList.add("lightbox-display-image");
+    lightboxClose.classList.add("lightbox-display-close");
   });
+});
+
+lightbox.addEventListener("click", () => {
+  let lightboxImage = document.getElementsByClassName("lightbox-image")[0];
+  let lightboxClose = document.getElementsByClassName("lightbox-close")[0];
+  lightbox.classList.remove("lightbox-display");
+  lightboxImage.classList.remove("lightbox-display-image");
+  lightboxClose.classList.remove("lightbox-display-close");
 });
