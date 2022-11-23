a6f1f56cbf8d96646d1bdbc7d9057344ad4fcc16
JackCHG <gongjackiegong@gmail.com>
2022-11-23 11:56:35 -0800

    Added let keyword

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,9 +1,9 @@
-images = document.getElementsByClassName("image");
+let images = document.getElementsByClassName("image");
 Array.from(images).forEach((image) => {
   console.log("Testing");
   image.addEventListener('click', (event) => {
     // console.log(event.target);
-    img = event.target;
+    let img = event.target;
     img.style.transform = "scale(1.5)";
     img.style.transition = "transform 0.25s ease";
   });
