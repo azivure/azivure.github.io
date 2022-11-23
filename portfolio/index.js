baadf0ebd73b242ec9a463b78bb24894c5f6e8cc
JackCHG <gongjackiegong@gmail.com>
2022-11-23 04:10:46 -0800

    Modified enlargeImage function

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,15 +1,5 @@
 const enlargeImage = (event) => {
   img = event.target;
-  // Function to set image dimensions
-  function enlargeImg() {
-      img.style.width = "60%";
-      img.style.height = "auto";
-      img.style.transition = "width 0.5s ease";
-  }
-  // Function to reset image dimensions
-  function resetImg() {
-      img.style.width = "40%";
-      img.style.height = "auto";
-      img.style.transition = "width 0.5s ease";
-  }
+  img.style.transform = "scale(1.5)";
+  img.style.transition = "transform 0.25s ease";
 };
