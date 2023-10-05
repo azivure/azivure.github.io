e32806e446c52488d357102dfbb56bdc2467264a
JackCHG <gongjackiegong@gmail.com>
2023-10-05 00:12:57 -0400

    Started adding images to portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,54 @@
+import { Unstable_Grid2 as Grid, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+type Image = {
+  description: string,
+  filename: string
+};
+
+const images = [
+  {
+    description: "This is a test",
+    filename: "lil_me.png"
+  }
+] as Image[];
+
 export default function Portfolio() {
   return (
-    <>
-      Hello world
-    </>
+    <Stack alignItems="center">
+    <Grid
+      container
+      direction="row"
+      spacing={2}
+    >
+      <Grid xs={12}>
+        <Typography
+          align="center"
+          variant="h1"
+          sx={{
+            fontWeight: "700"
+          }}
+        >
+          Projects
+        </Typography>
+      </Grid>
+
+      <Grid xs={5}>
+        <Box
+          component="img"
+          src="/lil_me.png"
+          sx={{
+            maxWidth: "250px",
+            maxHeight: "250px"
+          }}
+        />
+      </Grid>
+      <Grid xs={7}>
+        <Typography>
+          This is a description
+        </Typography>
+      </Grid>
+    </Grid>
+    </Stack>
   )
 }
